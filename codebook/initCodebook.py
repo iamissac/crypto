8e5734a65948d00d7f43259ab82043422fe12183
Issac Kim <iamissac@gmail.com>
2019-07-10 23:18:02 -0500

    Seperating functions

--- codebook/initCodebook.py
+++ codebook/initCodebook.py
@@ -1,10 +1,11 @@
 #This module will take the message and encode/decode with the predefined codebook.
-
 import sys
 import string
 import random
+import json
 
 def initCodeBook():
+
     #Creates a list of codeBook
     #Original list is alphabet, 26 chars.
     orig_list = []
@@ -20,19 +21,30 @@ def initCodeBook():
 
     print('Key List: ', key_list)
 
+    #Make an encryption dictionary with two lists above
     encryptor = dict(zip(orig_list, key_list))
 
     print('Encryptor', encryptor)
 
-    decryptor = {}
+    # #Save the above dictionary(encryptor) to a json file
+    enc_json = json.dumps(encryptor)
+    f = open("encryptor.json", "w")
+    f.write(enc_json)
+    f.close()
 
+    #Make a decryption dictionary by reversing the encryptor dictonary
+    decryptor = {}
     for k in encryptor:
         x = encryptor[k]
         decryptor[x] = k
 
+    # #Save the above dictionary(encryptor) to a json file
+    dec_json = json.dumps(decryptor)
+    f = open("decryptor.json", "w")
+    f.write(dec_json)
+    f.close()
 
     print('Decryptor:', decryptor)
 
 if __name__ == '__main__':
-    plaintext = 'hackers for charity'
     initCodeBook()
